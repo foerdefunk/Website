384fa8bebb8a5523ebf61fb68e4280fe7478c9a8
spralexx <rene.krick@gmx.de>
2015-09-01 03:16:36 +0200

    added leflet, jquery, and tons of according css

---
+++ assets/js/jquery.main.js
@@ -0,0 +1,59 @@
+/*
+    get json data for map
+*/
+$(document).ready(function() {
+
+    $.ajax({
+      url: 'assets/js/leaflet-0.7.3/leaflet.js',
+      dataType: 'script',
+      cache: true,
+      success: function() {
+
+        var myIcon = L.icon({
+            iconUrl: 'assets/images/logos/foerdefunk-logo-boje.svg',
+            iconRetinaUrl: 'assets/images/logos/foerdefunk-logo-boje.svg',
+            iconSize: [30, 32],
+            iconAnchor: L.Point[30, 32],
+            popupAnchor: L.Point[-15, -32]
+        });
+
+        var map = L.map('map', {
+            icon: myIcon,
+            scrollWheelZoom: false,
+            center: [54.790961, 9.435912],
+            zoom: 13
+        });
+
+        L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        var setNodeToMap = function(node) {
+            if(node.geo && node.flags.online) {
+                L.marker([node.geo[0], node.geo[1]], {icon: myIcon}).addTo(map).bindPopup('<h3>'+node.name+'</h3>');
+            }
+        }
+
+        $.ajax({
+            cache: false,
+            url: 'nodes.json',
+            dataType: 'json',
+            success: function(data) {
+                data.nodes.forEach(setNodeToMap);
+
+                var countNodes = 0;
+
+                for (var i = data.nodes.length - 1; i >= 0; i--) {
+                    if(data.nodes[i].flags.online && data.nodes[i].name != '') {
+                        countNodes++;
+                    }
+                };
+
+                $('#count-nodes').html(countNodes);
+            }
+        });
+
+      }
+    });
+
+});
